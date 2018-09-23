bde7d6eae99dd5786b80512347b0706bd4772549
Maxim Ilalov <maximum_sill@mail.ru>
2018-09-23 20:48:10 +0500

    hw06 is completed

--- Homework06/src/ru/itis/Homework06_1.java
+++ Homework06/src/ru/itis/Homework06_1.java
@@ -1,4 +1,25 @@
 package ru.itis;
 
-public class Homework05_1 {
+import java.util.Scanner;
+
+public class Homework06_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        String[] s = new String[n];
+        for (int i = 0; i < n; i++) {
+            s[i] = sc.next();
+        }
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n - 1 - i; j++) {
+                if (s[j].length() > s[j + 1].length()) ;
+                String buf = s[j];
+                s[j] = s[j + 1];
+                s[j + 1] = buf;
+            }
+        }
+        for (String k : s) {
+            System.out.println(k + " ");
+        }
+    }
 }
--- Homework06/src/ru/itis/Homework06_2.java
+++ Homework06/src/ru/itis/Homework06_2.java
@@ -1,4 +1,18 @@
 package ru.itis;
 
+import java.util.Scanner;
+
 public class Homework06_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        String full = "";
+        for (int i = 0; i < s.length(); i++) {
+            char sy = s.charAt(i);
+            if (sy != ' '){
+                full += sy;
+            }
+        }
+        System.out.println(full);
+    }
 }
